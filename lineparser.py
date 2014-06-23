a2a70bc684171ada1de433c5f3b5753169ec3011
Walter <walter@Walters-MacBook-Pro.local>
2014-06-23 07:57:43 -0600

    Modified Stout's python parser to use classes

--- lineparser.py
+++ lineparser.py
@@ -1,31 +1,54 @@
-#Opens file and seperates each data entry in to it's own section
-f = open("sample_data.txt")
-plotPoint = []
-for line in f:
-    data = line.split(",")
-    #Checks to see if it is in meters and if it isn't converts it to meters
-    if (data[2][-2:] == 'KM'):
-        string = data[2]
-        
-        #Checks for a decimal place and gets rid of it then adds the correct amount of zeroes
-        if string.find(".") != -1:
-            string = string[:string.find(".")] + string[string.find(".")+1:]
-            data[2] = string[:-2] + "00M" 
-        else:
-            data[2] = string[:-2] + "000M"
-            
-    plotPoint.append(data)
+import re
+import sys
+class Point:
+    #Constructor
+    def __init__(self, line=None):
+        self.centerpoint = ''
+        self.radius_meters = 0
+        self.name = ''
+        self.description = ''
+        if line is not None:
+            #remove leading and trailing whitespace
+            line = line.strip()
+            fields = line.split(',')
+            self.name = fields[0].strip()
+            self.centerpoint = fields[1].strip()
+            #use regular expression to seperate into 
+            #number and multiplyer
+            match = re.search(r"([\d\.]+)\s*(K?M)", fields[2] )
+            print '->%s< ->%s<-' % ( match.group(1), match.group(2))
+            self.radius_meters = float(match.group(1))
+            if match.group(2) == 'KM':
+                self.radius_meters *= 1000
+                self.radius_meters = int(self.radius_meters)
+            self.description = fields[3].strip('" ')
 
-#Goes through every possible combination and prints it out to make sure
-#everything got entered correctly
-for entry in plotPoint:
-    print(entry[0])
-    print(entry[1])
-    print(entry[2])
-    print(entry[3])
- 
- #Calling for data would be plotPoint[0][0]
- #X is the entry and Y is name, location, radius, description respectfully [0,1,2,3]
+    #Automatically convert to string if needed
+    def __str__(self):
+        #make use of python's printf style string formattting
+        result = "Name: %s\n" % self.name
+        result += "Description: %s\n" % self.description
+        result += "Centerpoint: %s\n" % self.centerpoint
+        result += "Radius: %d m" % self.radius_meters
+        return result
 
-#Pauses command line
-input()
+def main():
+    filename = "sample_data.txt"
+    plotPoint = []
+    if len(sys.argv) == 2:
+        #Let command line override default file if specified
+        filename = sys.argv[1]
+    f = open(filename, "r")
+    for line in f:
+        #Pass each line to the constructor and add the result to the array 
+        plotPoint.append(Point(line))
+        #Pauses command line
+    
+    for point in plotPoint:
+        print str(point)
+    dummy = raw_input()
+
+#by doing this our python script can both be imported as a module
+#and ran as a program.
+if __name__ == '__main__':
+    main()
